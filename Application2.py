ae42c689168b0dbef336d9a9072e7da9831f7203
tasnimassali <36279243+tasnimassali@users.noreply.github.com>
2021-09-06 15:02:52 +0100

    Get a list as an input and sort it jupyter notebook + script

---
+++ Application2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#define the list
+# creating an empty list
+numbers = []
+ 
+# number of elements as input
+n = int(input("Enter number of elements : "))
+ 
+# iterating till the range
+for i in range(0, n):
+    ele = int(input())
+ 
+    numbers.append(ele) # adding the element
+    
+#numbers = [11, 3, 7, 5, 2]
+print("the list ")
+print(numbers)
+# sort the list
+numbers.sort()
+number2 = numbers
+#print the sorted list
+print("sorted list")
+print(number2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
